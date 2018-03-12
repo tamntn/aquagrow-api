3897a13ea3e9169800477e1b98e27b7e01802ddb
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-03-12 01:06:48 -0500

    Update User Model to Hash Password Pre Save

--- models/User.js
+++ models/User.js
@@ -1,12 +1,25 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
 var Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 // Creating a Schema for users
 const userSchema = new Schema({
-    username: { type: String, required: true },
+    username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
 });
+
+// Hash password 'before' save
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+        user.password = bcrypt.hashSync(user.password, salt);
+        next();
+    } else {
+        next();
+    }
+})
 
 const User = mongoose.model('users', userSchema);
 
