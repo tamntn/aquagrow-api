3d342101c015fde1d52fab4962a7681a11d66251
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-03-11 22:31:28 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,24 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
 var Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 // Creating a Schema for users
 const userSchema = new Schema({
-    username: { type: String, required: true },
+    username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
 });
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+        user.password = bcrypt.hashSync(user.password, salt);
+        next();
+    } else {
+        next();
+    }
+})
 
 const User = mongoose.model('users', userSchema);
 
