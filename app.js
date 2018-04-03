b595f40a89e7855a05679b1c718b992d77048372
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-03 02:38:45 -0500

    Enable CORS

--- app.js
+++ app.js
@@ -37,6 +37,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+var allowCrossDomain = function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+}
+
+app.use(allowCrossDomain);
+
 // public folder setup
 // the root is at the 'public' folder itself
 app.use('/public', express.static(path.join(__dirname, 'public')));
