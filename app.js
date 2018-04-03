8dfa7ed67147679eb6488ca51fd1f9fb8c2bdac2
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-03 02:47:00 -0500

    Update CORS Middleware

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 var allowCrossDomain = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', "*");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 }
 
