db2a9c78aefcaeea18d494a2a14a916320c74a7a
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-04-11 15:51:23 -0500

    Delete Sub Notification Schemas

--- models/Notification.js
+++ models/Notification.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const NotificationTypeSchema = new Schema({
-    type: {
-        type: String,
-        required: true
-    }
-})
-
-const NotificationAspectSchema = new Schema({
-    aspect: {
-        type: String,
-        required: true
-    }
-})
-
 const NotificationSchema = new Schema({
     createdAt: {
         type: Date,
