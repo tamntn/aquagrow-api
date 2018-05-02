dc81aebd42a5d68355873f3af4a609da71a64d71
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-05-02 04:42:38 -0500

    Add socket.io server access to client app

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 var allowCrossDomain = function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', "http://localhost:3001");
+	res.header('Access-Control-Allow-Origin', "http://localhost:3001, https://app.aquagrow.life");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
